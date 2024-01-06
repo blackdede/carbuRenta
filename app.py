b9a870e2943c04cc4e55ca159760e6dbcea0d6e8
Denis <dd.denis.delmas@gmail.com>
2024-01-06 22:16:32 +0100

    Edit few graphs and added hover infos

--- app.py
+++ app.py
@@ -1,36 +1,14 @@
+from datetime import timedelta, datetime
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-heatmap_dataframe = pd.read_json("graph_data/data.json")
-
 app = Dash(__name__)
 
-# Add a list of fuel types available in your dataset
-fuel_types = ["Gazole", "SP95", "E85", "E10", "SP98"]
-
-days_len = len(heatmap_dataframe['stations'][0]['carburants'][fuel_types[0]])
-
-# Create dropdown for fuel selection
-fuel_dropdown = dcc.Dropdown(
-    id='fuel-dropdown',
-    options=[{'label': fuel, 'value': fuel} for fuel in fuel_types],
-    value=fuel_types[0],  # Default value
-)
-
-# Create slider for selecting the index of the fuel prices list
-slider = dcc.Slider(
-    id='price-slider',
-    min=0,
-    max=days_len - 1,
-    marks={i: str(i) for i in range(len(heatmap_dataframe['stations'][0]['carburants'][fuel_types[0]]))},
-    value=0,  # Default value
-)
-
-def get_icicle():
+def get_piechart(selected_stations):
     """
-    Carte des noms de stations, afin de voir les marques qui ressortent le plus
+    Diagramme en camembert pour afficher la distribution des stations par marque
     """
     station_names = {}
 
@@ -46,36 +24,43 @@ def get_icicle():
             'count': list(station_names.values())
         }
     )
+    
+    # Sort the dataframe by count in descending order
+    df = df.sort_values(by='count', ascending=False)
+    
 
-    fig = px.icicle(df, path=['name'], values='count', title='Nombre de stations par marque')
-    fig.update_traces(root_color="lightgrey")
-    fig.update_layout(margin=dict(l=40, r=40, t=40, b=40))
+    # Limit the dataframe to the top X rows
+    df = df.head(selected_stations)
 
+    df['hover_text'] = (
+        'Nom de la marque: ' + df['name'] +
+        '<br>Nombre de stations: ' + df['count'].astype(str)
+    )
+
+    fig = px.pie(df, names='name', values='count', title='Distribution des stations par marque',
+                 hover_data=['hover_text'], hover_name='name')
+
+    # Update the hover template
+    fig.update_traces(
+        hovertemplate="Nom de la marque: %{label} <br>Nombre de stations: %{value} <br>Pourcentage: %{percent}",
+    )
     return fig
 
-app.layout = html.Div([
-    html.H1(children='Carburenta - Prix des carburants en France 🐀', style={'textAlign': 'center'}),
-    
-    # Add dropdown and slider to the layout
-    fuel_dropdown,
-    slider,
-    
-    # Update the graph based on dropdown and slider values
-    dcc.Graph(id='heatmap'),
-    dcc.Graph(id='histogram'),
-    dcc.Graph(id='piechart'),
-    dcc.Graph(id='markersmap'),
-    dcc.Graph(id='icicle', figure=get_icicle())
-])
+@app.callback(
+    Output('piechartNameStations', 'figure'),
+    [Input('stations-dropdown', 'value')]
+)
+def update_piechart(selected_stations):
+    return get_piechart(selected_stations)
 
 @app.callback(
-    Output('piechart', 'figure'),
-    [Input('price-slider', 'value')]
+    Output('piechartPriceStations', 'figure'),
+    [Input('date-picker', 'date')]
 )
-def update_piechart(selected_price_index):
-    """
-    Grpahique en camembert qui montre le prix moyen de chaque carburant pour le jour selectionné
-    """
+def update_piechart(selected_date):
+    selected_date = datetime.strptime(selected_date, "%Y-%m-%d")
+    selected_price_index = (selected_date - datetime(2023, 1, 1)).days
+
     gas_average_prices = [0] * len(fuel_types)
     stations_lens = [0] * len(fuel_types)
 
@@ -95,8 +80,7 @@ def update_piechart(selected_price_index):
         }
     )
 
-    fig = px.pie(df, values='prix', names='carburant', title=f'Prix moyen des carburants il y à {days_len - selected_price_index} jour(s)')
-
+    fig = px.pie(df, values='prix', names='carburant', title=f'Prix moyen des carburants le {selected_date.strftime("%Y-%m-%d")}')
     return fig
 
 @app.callback(
@@ -118,24 +102,32 @@ def update_histogram(selected_fuel):
     for i in range(prices_len):
         average_prices[i] /= stations_len
 
+    # Convert the day index to date
+    start_date = datetime(2023, 1, 1)  # Assuming the data starts on January 1, 2023
+    date_labels = [(start_date + timedelta(days=i)).strftime("%Y-%m-%d") for i in range(prices_len)]
+
     df = pd.DataFrame(
         {
+            'date': date_labels,
             'price': average_prices
         }
     )
 
-    fig = px.bar(df, y="price", 
+    fig = px.bar(df, x="date", y="price",
                  title=f'Histogramme des prix du {selected_fuel}',
-                 labels={'price': 'Prix moyen (€)', 'index': 'il y a un an à aujourd\'hui'})
+                 labels={'price': 'Prix moyen (€)', 'date': 'Date'})
 
     return fig
 
 # Define callback to update the heatmap based on dropdown and slider values
 @app.callback(
     Output('heatmap', 'figure'),
-    [Input('fuel-dropdown', 'value'), Input('price-slider', 'value')]
+    [Input('fuel-dropdown', 'value'), Input('date-picker', 'date')]
 )
-def update_heatmap(selected_fuel, selected_price_index):
+def update_heatmap(selected_fuel, selected_date):
+    # Convert the selected date to the corresponding index
+    selected_date = datetime.strptime(selected_date, "%Y-%m-%d")
+    selected_price_index = (selected_date - datetime(2023, 1, 1)).days
     # Afficher sur une heatmap de densitée les stations les moins chères
     # Il s'agit d'une heatmap de densité, on ne peut pas régler la couleur en fonction du prix. On ne met donc pas toutes les stations car la carte est trop similaire peu importe le jour selectionné.
     
@@ -177,15 +169,19 @@ def update_heatmap(selected_fuel, selected_price_index):
                         zoom=4,
                         mapbox_style="open-street-map",
                         title=f'Moitié des stations avec le prix du {selected_fuel} le plus bas',
-                        hover_name='id')
+                        hover_name='id',
+                        labels={'price': 'Prix moyen (€)'})
     
     return fig
 
 @app.callback(
     Output('markersmap', 'figure'),
-    [Input('fuel-dropdown', 'value'), Input('price-slider', 'value')]
+    [Input('fuel-dropdown', 'value'), Input('date-picker', 'date')]
 )
-def update_markersmap(selected_fuel, selected_price_index):
+def update_markersmap(selected_fuel, selected_date):
+    selected_date = datetime.strptime(selected_date, "%Y-%m-%d")
+    selected_price_index = (selected_date - datetime(2023, 1, 1)).days
+    
     default_price = 0.0
     bound_top_left = (48.90415749721205, 2.450568379885376)
     bound_bottom_right = (48.79039931828495, 2.682282385717415)
@@ -207,45 +203,86 @@ def update_markersmap(selected_fuel, selected_price_index):
     ],
     columns=['latitude', 'longitude', 'price'])
 
-    fig = go.Figure(data=go.Scattergeo(
-        locationmode="ISO-3",
-        lon = df['longitude'],
-        lat = df['latitude'],
-        text = df['price'],
-        mode = 'markers',
-        marker = dict(
-            size = 8,
-            opacity = 0.8,
-            reversescale = True,
-            autocolorscale = False,
-            symbol = 'square',
-            line = dict(
-                width=1,
-                color='rgba(102, 102, 102)'
-            ),
-            colorscale = 'Blues',
-            cmin = 0,
-            color = df['price'],
-            cmax = df['price'].max(),
-            colorbar_title=f'Prix du {selected_fuel}'
-        )))
+    fig = go.Figure(go.Scattermapbox(
+        lat=df['latitude'],
+        lon=df['longitude'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=8,
+            opacity=0.8,
+            color=df['price'],
+            colorscale='bluered',
+            cmin=df['price'].min(),
+            cmax=df['price'].max(),
+            colorbar=dict(title=f'Prix du {selected_fuel}')
+        ),
+        text=df['price']
+    ))
 
     fig.update_layout(
-        title = 'Carte des stations autour de ESIEE Paris, colorées par prix',
-        geo = dict(
-            scope='europe',
-            showland = True,
-            landcolor = "rgb(250, 250, 250)",
-            subunitcolor = "rgb(217, 217, 217)",
-            countrycolor = "rgb(217, 217, 217)",
-            countrywidth = 0.5,
-            subunitwidth = 0.5, 
-            projection_scale=400,
+        title='Carte des stations autour de ESIEE Paris, colorées par prix',
+        mapbox=dict(
+            style="carto-positron",
             center=dict(lat=48.847, lon=2.549),
+            zoom=12,
         ),
     )
 
     return fig
+
+heatmap_dataframe = pd.read_json("graph_data/data.json")
+
+
+
+# Add a list of fuel types available in your dataset
+fuel_types = ["Gazole", "SP95", "E85", "E10", "SP98"]
+
+days_len = len(heatmap_dataframe['stations'][0]['carburants'][fuel_types[0]])
+
+# Create dropdown for fuel selection
+fuel_dropdown = dcc.Dropdown(
+    id='fuel-dropdown',
+    options=[{'label': fuel, 'value': fuel} for fuel in fuel_types],
+    value=fuel_types[0],  # Default value
+)
+
+# Create slider for selecting the index of the fuel prices list
+date_picker = dcc.DatePickerSingle(
+    id='date-picker',
+    min_date_allowed=datetime(2023, 1, 1),
+    max_date_allowed=datetime(2023, 12, 31),
+    initial_visible_month=datetime(2023, 12, 31),
+    date=datetime(2023, 12, 31).strftime("%Y-%m-%d")  # Use string representation
+)
+
+stations_dropdown = dcc.Dropdown(
+    id='stations-dropdown',
+    options=[
+        {'label': 'Top 5', 'value': 5},
+        {'label': 'Top 10', 'value': 10},
+        {'label': 'Top 15', 'value': 15},
+        {'label': 'Top 20', 'value': 20},
+        {'label': 'Top 25', 'value': 25},
+    ],
+    value=15  # Default value
+)
+
+app.layout = html.Div([
+    html.H1(children='Carburenta - Prix des carburants en France 🐀', style={'textAlign': 'center'}),
+    
+    # Add dropdown and date picker to the layout
+    fuel_dropdown,
+    date_picker,
+    
+    # Update the graph based on dropdown and date picker values
+    dcc.Graph(id='heatmap'),
+    dcc.Graph(id='histogram'),
+    dcc.Graph(id='piechartPriceStations'),
+    dcc.Graph(id='markersmap'),
+    stations_dropdown,
+    dcc.Graph(id='piechartNameStations', figure=get_piechart(15))
+
+])
 
 if __name__ == '__main__':
     app.run(debug=True)
